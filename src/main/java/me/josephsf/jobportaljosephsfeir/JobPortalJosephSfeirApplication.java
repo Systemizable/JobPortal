b417a8d40ab22f308d0643f2ac44fa3e23baff5c
Systemizable <202311768@ua.edu.lb>
2025-05-10 16:36:10 +0300

    Massive

--- src/main/java/me/josephsf/jobportaljosephsfeir/JobPortalJosephSfeirApplication.java
+++ src/main/java/me/josephsf/jobportaljosephsfeir/JobPortalJosephSfeirApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobPortalJosephSfeirApplication {
 
     public static void main(String[] args) {
+        // Log the MongoDB URI at startup
+        System.out.println("=== MONGODB_URI Environment Variable ===");
+        System.out.println(System.getenv("MONGODB_URI"));
+        System.out.println("======================================");
+
         SpringApplication.run(JobPortalJosephSfeirApplication.class, args);
     }
-
 }
