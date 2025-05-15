76ae60a856c87d2d3aa2c3b2aad8c178aa6f862b
Systemizable <202311768@ua.edu.lb>
2025-05-15 18:39:16 +0300

    Fix SDKMAN path in build script

--- build-javadoc.sh
+++ build-javadoc.sh
@@ -4,8 +4,14 @@ set -e
 # Install Java
 echo "Installing Java..."
 curl -s "https://get.sdkman.io" | bash
-source "C:\Users\Joseph/.sdkman/bin/sdkman-init.sh"
-sdk install java 21.0.2-open
+
+# Use the correct path for the sdkman-init.sh file on Render
+echo "Sourcing SDKMAN..."
+source "/opt/render/.sdkman/bin/sdkman-init.sh"
+
+# Accept license for Java
+echo "Installing Java 21..."
+echo "Y" | sdk install java 21.0.2-open
 
 # Make gradlew executable
 echo "Making gradlew executable..."
